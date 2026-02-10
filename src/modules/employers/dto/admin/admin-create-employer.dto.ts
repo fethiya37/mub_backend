3c845773d30f74bf39677c1a0aac048f26f955b1
Fethiya Seid <110985471+fethiya37@users.noreply.github.com>
2026-02-10 14:03:53 +0300

    issue fixed b/c of multipart/formdata

--- src/modules/employers/dto/admin/admin-create-employer.dto.ts
+++ src/modules/employers/dto/admin/admin-create-employer.dto.ts
@@ -1,10 +1,12 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsBoolean, IsOptional } from 'class-validator';
 import { EmployerRegisterDto } from '../employer-register.dto';
 
 export class AdminCreateEmployerDto extends EmployerRegisterDto {
   @ApiPropertyOptional({ example: false })
   @IsOptional()
+  @Type(() => Boolean)
   @IsBoolean()
   autoApprove?: boolean;
 }
